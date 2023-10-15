a599ed192a1f52b03d2f8398688740891fb6bc9b
LemonjamesD <lemon@lemonjamesd.com>
2023-10-15 16:55:10 -0400

    Fixed resource test

--- tests/resource.rs
+++ tests/resource.rs
@@ -8,12 +8,12 @@ pub struct TestResource {
 impl Resource for TestResource {}
 
 pub fn test_resource(world: &World) {
-    let mut resource = world.get_resource_read::<TestResource>().unwrap();
+    let mut resource = world.get_resource_write::<TestResource>().unwrap();
     println!("{}", resource.x);
-    // resource.x += 10;
+    resource.x += 10;
 }
 
 #[test]
 pub fn create_resource() {
-    let world = World::new().add_system(test_resource).add_resource(TestResource { x: 100 }).run();
+    let world = World::new().add_system(test_resource).add_resource(TestResource { x: 100 }).start().single_step().single_step();
 }
